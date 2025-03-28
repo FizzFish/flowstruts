ffbd37b1eb038964b5bae1a4c393261f6a4dee78
Tim Lange <mail@tim-lange.me>
2025-03-28 16:46:51 +0100

    Fix listIrrelevantItemTest testcase

--- soot-infoflow-summaries/test/soot/jimple/infoflow/test/methodSummary/junit/SummaryTaintWrapperTests.java
+++ soot-infoflow-summaries/test/soot/jimple/infoflow/test/methodSummary/junit/SummaryTaintWrapperTests.java
@@ -338,7 +338,7 @@ public abstract class SummaryTaintWrapperTests extends BaseSummaryTaintWrapperTe
 
 	@Test(timeout = 30000)
 	public void listIrrelevantItemTest() {
-		testFlowForMethod("<soot.jimple.infoflow.test.methodSummary.ApiClassClient: void listIrrelevantItemTest()>", 1);
+		testNoFlowForMethod("<soot.jimple.infoflow.test.methodSummary.ApiClassClient: void listIrrelevantItemTest()>");
 	}
 
 	@Test
